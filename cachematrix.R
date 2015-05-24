15cda354a8e7d02e9730bade9c068e032dbbccf0
ealeph <ealeph@gmail.com>
2015-05-25 00:18:33 +0300

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The function makeCacheMatrix creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(m=matrix()) {
+  inv<-NULL
+  set<-function(x) {
+    m <<-x
+    inv <<-null
+  }
+  get<-function()m
+  setinv <-function(solve) inv<<-solve
+  getinv <-function() inv
+  list(set=set, get=get,
+       setinv=setinv,
+       getinv=getinv)
 }
 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(m,...){
+  inv<-m$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data<-m$get()
+  inv<-solve(data,...)
+  m$setinv(inv)
+  return(inv) ## Return a matrix that is the inverse of 'm'
 }
+
+checking:
+  > m<-matrix(1:4,2,2)
+> print(m)
+[,1] [,2]
+[1,]    1    3
+[2,]    2    4
+> trx=makeCacheMatrix(m)
+> trx$get()
+[,1] [,2]
+[1,]    1    3
+[2,]    2    4
+> cacheSolve(trx)
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> cacheSolve(trx)
+getting cached data
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+
